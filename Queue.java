447c22016fa0dd7b536517806e853ae5ee990ecb
Dylan Miracle <dylan.miracle@gmail.com>
2019-03-18 16:01:13 -0500

    first

--- Queue.java
+++ Queue.java
@@ -41,4 +41,33 @@ public class Queue<E> {
         }
         return i;
     }
+    class Node {
+        private Object data;
+        Node next;
+        public Node(){
+            
+        }
+        public Node(Object data){
+            this.data = data;
+            this.next = null;
+        }
+        public Node(Object data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+        // getters
+        public Object getData(){
+            return data;
+        }
+        public Node getNext(){
+            return next;
+        }
+        // setters
+        public void setData(Object data){
+            this.data = data;
+        }
+        public void setNext(Node next){
+            this.next = next;
+        }
+    }
 }
